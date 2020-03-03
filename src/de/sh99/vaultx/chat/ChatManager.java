587f3505c7454df80e817bde0a5fcee06f7d48bf
Mooon1 <xenodev@outlook.de>
2020-03-03 19:06:08 +0100

    well put methods to wrong file

--- src/de/sh99/vaultx/chat/ChatManager.java
+++ src/de/sh99/vaultx/chat/ChatManager.java
@@ -12,57 +12,7 @@ import java.util.UUID;
 
 public interface ChatManager
 {
-    public String getName();
 
-    public boolean isEnabled();
-
-    public boolean isSuperPermsCompatible();
-
-    public boolean has(@NotNull CommandSender sender, @NotNull String permission);
-
-    public boolean add(@NotNull OfflinePlayer offlinePlayer, @NotNull String permission, @Nullable World world);
-
-    public boolean add(@NotNull Player player, @NotNull String permission, @Nullable World world);
-
-    public boolean remove(@NotNull OfflinePlayer offlinePlayer, @NotNull String permission, @Nullable World world);
-
-    public boolean remove(@NotNull Player player, @NotNull String permission, @NotNull World world);
-
-    public boolean addTransient(@NotNull Player player, @NotNull String permission, @Nullable World world);
-
-    public boolean addTransient(@NotNull OfflinePlayer offlinePlayer, @NotNull String permission, @Nullable World world);
-
-    public boolean removeTransient(@NotNull Player player, @NotNull String permission, @Nullable World world);
-
-    public boolean removeTransient(@NotNull OfflinePlayer offlinePlayer, @NotNull String permission, @Nullable World world);
-
-    public boolean groupHas(@NotNull String group, @NotNull String permission, @Nullable World world);
-
-    public boolean groupRemove(@NotNull String group, @NotNull String permission, @Nullable World world);
-
-    public boolean isMember(@NotNull String group, @NotNull Player player, @Nullable World world);
-
-    public boolean isMember(@NotNull String group, @NotNull OfflinePlayer player, @Nullable World world);
-
-    public boolean addMember(@NotNull String group, @NotNull Player player, @Nullable World world);
-
-    public boolean addMember(@NotNull String group, @NotNull OfflinePlayer player, @Nullable World world);
-
-    public boolean removeMember(@NotNull String group, @NotNull Player player, @Nullable World world);
-
-    public boolean removeMember(@NotNull String group, @NotNull OfflinePlayer player, @Nullable World world);
-
-    public List<String> getGroupsByPlayer(@NotNull Player player, @Nullable World world);
-
-    public List<String> getGroupsByPlayer(@NotNull OfflinePlayer offlinePlayer, @Nullable World world);
-
-    public String getPrimaryGroup(@NotNull Player player, @Nullable World world);
-
-    public String getPrimaryGroup(@NotNull OfflinePlayer offlinePlayer, @Nullable World world);
-
-    public List<String> getGroups();
-
-    public boolean hasGroupSupport();
 
 
 }
